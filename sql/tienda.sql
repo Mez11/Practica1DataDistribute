6c77cd18aa40cb85f96bc8bb4fd5f39d302d91e2
Misael Alanis <misaelarturo@gmail.com>
2017-02-19 00:12:22 -0600

    Agrega el script de creación de BD

---
+++ sql/tienda.sql
@@ -0,0 +1,299 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tienda
+-- ------------------------------------------------------
+-- Server version	5.7.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `compradetalle`
+--
+
+DROP SCHEMA IF EXISTS `tienda`;
+CREATE DATABASE `tienda`;
+USE `tienda`;
+
+DROP TABLE IF EXISTS `compradetalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `compradetalle` (
+  `idCompraDetalle` int(11) NOT NULL AUTO_INCREMENT,
+  `idInventario` int(11) NOT NULL,
+  `idVenta` int(11) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `subtotal` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`idCompraDetalle`),
+  KEY `idInventario` (`idInventario`),
+  KEY `idVenta` (`idVenta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compradetalle`
+--
+
+LOCK TABLES `compradetalle` WRITE;
+/*!40000 ALTER TABLE `compradetalle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compradetalle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `disco`
+--
+
+DROP TABLE IF EXISTS `disco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `disco` (
+  `idInventario` int(11) NOT NULL AUTO_INCREMENT,
+  `numCanciones` int(11) DEFAULT NULL,
+  `interprete` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`idInventario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `disco`
+--
+
+LOCK TABLES `disco` WRITE;
+/*!40000 ALTER TABLE `disco` DISABLE KEYS */;
+/*!40000 ALTER TABLE `disco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `imagen`
+--
+
+DROP TABLE IF EXISTS `imagen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `imagen` (
+  `idImagen` int(11) NOT NULL AUTO_INCREMENT,
+  `imagen` varchar(450) DEFAULT NULL,
+  PRIMARY KEY (`idImagen`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `imagen`
+--
+
+LOCK TABLES `imagen` WRITE;
+/*!40000 ALTER TABLE `imagen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `imagen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventario`
+--
+
+DROP TABLE IF EXISTS `inventario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventario` (
+  `idInventario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(250) DEFAULT NULL,
+  `categoria` varchar(200) DEFAULT NULL,
+  `anio` varchar(15) DEFAULT NULL,
+  `numPiezas` int(11) DEFAULT NULL,
+  `precioCompra` decimal(10,0) DEFAULT NULL,
+  `precioVenta` decimal(10,0) DEFAULT NULL,
+  `idImagen` int(11) NOT NULL,
+  PRIMARY KEY (`idInventario`),
+  KEY `idImagen` (`idImagen`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventario`
+--
+
+LOCK TABLES `inventario` WRITE;
+/*!40000 ALTER TABLE `inventario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pago`
+--
+
+DROP TABLE IF EXISTS `pago`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pago` (
+  `idPago` int(11) NOT NULL AUTO_INCREMENT,
+  `pago` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`idPago`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pago`
+--
+
+LOCK TABLES `pago` WRITE;
+/*!40000 ALTER TABLE `pago` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pago` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pelicula`
+--
+
+DROP TABLE IF EXISTS `pelicula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pelicula` (
+  `idInventario` int(11) NOT NULL AUTO_INCREMENT,
+  `idioma` varchar(60) DEFAULT NULL,
+  `subtitulos` varchar(200) DEFAULT NULL,
+  `formato` varchar(150) DEFAULT NULL,
+  `director` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`idInventario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pelicula`
+--
+
+LOCK TABLES `pelicula` WRITE;
+/*!40000 ALTER TABLE `pelicula` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pelicula` ENABLE KEYS */;
+UNLOCK TABLES;
+--
+-- Table structure for table `tarjeta`
+--
+
+DROP TABLE IF EXISTS `tarjeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tarjeta` (
+  `idTarjeta` int(11) NOT NULL AUTO_INCREMENT,
+  `numero` varchar(50) DEFAULT NULL,
+  `tipo` varchar(30) DEFAULT NULL,
+  `idUsuario` int(11) NOT NULL,
+  PRIMARY KEY (`idTarjeta`),
+  KEY `idUsuario` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tarjeta`
+--
+
+LOCK TABLES `tarjeta` WRITE;
+/*!40000 ALTER TABLE `tarjeta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tarjeta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) DEFAULT NULL,
+  `apellidoP` varchar(20) DEFAULT NULL,
+  `apellidoM` varchar(20) DEFAULT NULL,
+  `fech_na` date DEFAULT NULL,
+  `correo` varchar(150) DEFAULT NULL,
+  `sexo` varchar(2) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `tipo` varchar(10) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vendedor`
+--
+
+DROP TABLE IF EXISTS `vendedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vendedor` (
+  `idUsuario` int(11) NOT NULL,
+  `hora_entrada` time DEFAULT NULL,
+  `hora_comida` time DEFAULT NULL,
+  `hora_salida` time DEFAULT NULL,
+  `valComision` decimal(10,0) DEFAULT NULL,
+  `salarioBase` decimal(10,0) DEFAULT NULL,
+  `ventas` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vendedor`
+--
+
+LOCK TABLES `vendedor` WRITE;
+/*!40000 ALTER TABLE `vendedor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vendedor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `venta`
+--
+
+DROP TABLE IF EXISTS `venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `venta` (
+  `idVenta` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `total` decimal(10,0) DEFAULT NULL,
+  `idPago` int(11) NOT NULL,
+  `idTarjeta` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idVenta`),
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idPago` (`idPago`),
+  KEY `idTarjeta` (`idTarjeta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `venta`
+--
+
+LOCK TABLES `venta` WRITE;
+/*!40000 ALTER TABLE `venta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `venta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-18 12:27:42
