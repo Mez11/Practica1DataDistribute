6c2f4d343742712e09c5b202830743b14328b8d1
Joscelyn Meztli <meztli.swim@gmail.com>
2017-02-19 15:46:05 -0600

    Elimina comentarios

--- prBd.php
+++ prBd.php
@@ -7,13 +7,6 @@
         printf( "Correcto" );
     }
 
-    //ñaja 
-    //Viste los pasos para checar cual fue el error?
-    //1- Determinar si habia conexion entre PHP y MySQL a traves del phpinfo
-    //2- Leer los logs de apache (/var/log/apache2/error.log)
-    //3-Ver en especifico que causaba
-    //PD: Ya viste que VSCode trae muchas instrucciones de php?
-    //PD2: Cuack
 ?>
 
 
