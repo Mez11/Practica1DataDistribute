4a09bb3ba9b5aa8bc12e31fb767882448df75c42
Joscelyn Meztli <meztli.swim@gmail.com>
2017-02-20 00:01:37 -0600

    Corrige el PHP para consulta

--- php/consultarInventario.php
+++ php/consultarInventario.php
@@ -1,16 +1,21 @@
+<?php
+	require_once 'DAOInventario.php';
+?>
 <html>
 <head>
 </head>
 <body>
 
 	<?php
+		//esta pagina para que es??? para consultar el inventario osea vizualizar los productos
+		//en el method del formulario (POST) estas definiendo que los subsecuentes parametros
+		//se enviaran por POST.
 		echo "<form action='actualiza.php?idP=".$_GET["idP"]."' method='POST'>";
-		require_once 'DAOInventario.php';
+		//Como vas a consultar (sin modificar en esta peticion) esta bien pedir los parametros
+		//por GET (mi_pagina.php?mi_parametro=valor)
 		$idInventario = $_GET["idP"];
-		$idImagen = $_GET["idIm"];
-		$idCategoria = $_GET["idCat"];
 		$dao = new DAOProducto();
-		$inventario = $dao->consultarUnInventario($idInventario,$idImagen,$idCategoria);
+		$inventario = $dao->consultarUnInventario( $idInventario );
 		echo "<p>Nombre:</p><input type='text' name='nombre' value=".$inventario->getNombre()." />";
 		echo "<p>Anio:</p><input type='text' name='anio' value=".$inventario->getAnio()." />";
 		echo "<p>NumeroDePiezas:</p><input type='text' name='numPiezas' value=".$inventario->getNumPiezas()." />";
@@ -18,6 +23,7 @@
 		echo "<p>precioVenta:</p><input type='text' name='precioVenta' value=".$inventario->getPrecioVenta()." />";
 		echo "<br><br>";
 	?>
+	<!-- ñaja tengo que decir... todo esto era mas bonito (visualmente hablando, es decir, diseño) con JSF -->
 		<input type="submit" name="Editar" value="Editar"/>
 		<input type="submit" name="Cancelar" value="Eliminar"/>
 	</form>
