bc14abdc291ed9a5e639916e08fe9c73d9e3e4e8
Misael Alanis <misaelarturo@gmail.com>
2017-02-20 22:54:18 -0600

    termina el git merge

--- php/formularioInventario.php
+++ php/formularioInventario.php
@@ -5,21 +5,14 @@
 <head>
 </head>
 <body>
-<<<<<<< HEAD
 	<form action="agregaInventario.php" method="POST">
-		<p>Nombre:</p><input type="text" name="nombre"/>
-		<p>Precio de compra:</p><input type="text" name="precio"/>
-		<p><label>Imagen</label> </p>
-=======
-	<form action="prueba2.php" method="POST">
-
 		<p>*Nombre:</p><input type="text" name="nombre"/>
 		<p>*Anio:</p><input type="text" name="anio"/>
 		<p>*Numero de piezas:</p><input type="text" name="numPiezas">
 		<p>*Precio de compra:</p><input type="text" name="precioCompra"/>
 		<p>*Precio de venta:</p><input type="text" name="precioVenta"/>
 		<p><label>Imagen:</label> </p>
->>>>>>> 72641b27b5aa1843542ce2ce3c7f821533ee2e4d
+
 		<input type="text" name="id_imagen"/>
 		<p><label>*Categoria:</label></p>
 		<input type="text" name="id_categoria"/>
